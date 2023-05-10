6f3fa4d1a07ea0ec6d209f6ec72b656cc804d4e2
mayurkanade44 <mayurkanade44@gmail.com>
2023-05-10 17:49:40 +0530

    backend get & update user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,9 @@ import User from "../models/User.js";
 export const registerUser = async (req, res, next) => {
   const { name, email, password } = req.body;
   try {
+    if (!password || !email || !name)
+      throw new Error("Please provide all values");
+
     let user = await User.findOne({ email });
     if (user) throw new Error("User already exist");
 
@@ -19,6 +22,73 @@ export const registerUser = async (req, res, next) => {
       },
       msg: `Welcome ${user.name}`,
     });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!password || !email) throw new Error("Please provide all values");
+
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("Invalid credentials");
+
+    if (!(await user.comparePassword(password)))
+      throw new Error("Invalid credentials");
+
+    return res.status(200).json({
+      user: {
+        userId: user._id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        admin: user.admin,
+        token: await user.createJWT(),
+      },
+      msg: `Welcome ${user.name}`,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.userId);
+
+    if (user) return res.status(200).json({ user });
+
+    let error = new Error("User Not Found");
+    error.statusCode = 404;
+    next(error);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  const { name, email, avatar } = req.body;
+  try {
+    let user = await User.findById(req.user.userId);
+
+    if (!user) throw new Error("User does not exists");
+
+    if (email !== user.email) {
+      const alreadyExists = await User.findOne({ email });
+      if (alreadyExists) throw new Error("Email id already exists");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await user.save();
+
+    return res.status(200).json({ user, msg: "Profile successfully updated" });
   } catch (error) {
     console.log(error);
     next(error);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,19 @@
-import express from 'express'
-import { registerUser } from '../controllers/userController.js'
-const router = express.Router()
+import express from "express";
+import { authenticateUser } from "../middleware/auth.js";
+import {
+  getUser,
+  loginUser,
+  registerUser,
+  updateUser,
+} from "../controllers/userController.js";
 
+const router = express.Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router
+  .route("/profile")
+  .get(authenticateUser, getUser)
+  .patch(authenticateUser, updateUser);
 
-export default router
+export default router;
